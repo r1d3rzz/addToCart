966306878b01723a41e6948eafad3823caf76dce
rider <myintthwaykhine127@gmail.com>
2022-02-01 11:00:17 +0630

    increate inCart total Number in localStorage

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
 const cartBtn = document.querySelectorAll(".cartBtn");
 const cartNumberText = document.querySelector(".cartNumber");
 
+const products = [
+  {
+    name: "Addidas Hoddie",
+    tag: "addidasHoddie",
+    price: 25,
+    inCart: 0,
+  },
+  {
+    name: "Black Hoddie",
+    tag: "blackHoddie",
+    price: 25,
+    inCart: 0,
+  },
+  {
+    name: "Forest Hoddie",
+    tag: "forestHoddie",
+    price: 30,
+    inCart: 0,
+  },
+  {
+    name: "White Hoddie",
+    tag: "whiteHoddie",
+    price: 10,
+    inCart: 0,
+  },
+];
+
 for (let i = 0; i < cartBtn.length; i++) {
   cartBtn[i].addEventListener("click", function () {
-    cartNumber();
+    cartNumber(products[i]);
   });
 }
 
-function cartNumber() {
+function cartNumber(product) {
   let cartNumber = localStorage.getItem("cartNumber");
   cartNumber = Number(cartNumber);
   if (cartNumber) {
@@ -17,6 +44,29 @@ function cartNumber() {
     localStorage.setItem("cartNumber", 1);
     cartNumberText.textContent = 1;
   }
+
+  setItemsProducts(product);
+}
+
+function setItemsProducts(product) {
+  let setItemsProducts = localStorage.getItem("setItemsProducts");
+  setItemsProducts = JSON.parse(setItemsProducts);
+
+  if (setItemsProducts != null) {
+    if (setItemsProducts[product.tag] == undefined) {
+      setItemsProducts = {
+        ...setItemsProducts,
+        [product.tag]: product,
+      };
+    }
+    setItemsProducts[product.tag].inCart += 1;
+  } else {
+    product.inCart = 1;
+    setItemsProducts = {
+      [product.tag]: product,
+    };
+  }
+  localStorage.setItem("setItemsProducts", JSON.stringify(setItemsProducts));
 }
 
 function initFunction() {
